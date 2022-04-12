021bf95cd2008fb2f16b3377f57aed5d142216ee
upship-kim <tkdqook@gmail.com>
2022-04-13 02:17:40 +0900

    server url 변경

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const client = axios.create({
-    baseURL: "http://localhost:4000/",
+    baseURL: "https://upship-survey.herokuapp.com/",
     withCredentials: true,
 });
